0577b1f310ef69c812360f59cf5e046aacda0c45
Rudolfs Bundulis <rudolfs.bundulis@gmail.com>
2019-12-03 12:07:46 +0200

    Initial Commit

---
+++ WebRTC Player/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebRTC Player
+//
+//  Created by Rudolfs Bundulis on 03/12/2019.
+//  Copyright © 2019 Rudolfs Bundulis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ WebRTC Player/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  WebRTC Player
+//
+//  Created by Rudolfs Bundulis on 03/12/2019.
+//  Copyright © 2019 Rudolfs Bundulis. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
